205749cc2cb6d9fbe052291a2982e9516b03ac17
Matheus Sartori <ms.sartori@outlook.com.br>
2024-04-22 10:28:56 -0300

    feat: update url and organization project names

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,15 +8,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ts-algorithms-docs.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'matheussartori', // Usually your GitHub org/user name.
+  projectName: 'ts-algorithms-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',

9ab11a33e88903ba3a2f6663a4963dbe95e0e391
Matheus Sartori <ms.sartori@outlook.com.br>
2024-04-22 10:59:32 -0300

    feat: update discord invite link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -94,16 +94,8 @@ const config: Config = {
           title: 'Community',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
               label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
+              href: 'https://discord.gg/XmkwqGHppr',
             },
           ],
         },

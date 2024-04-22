f1ed224f3399cdcb1e31e842f5ff1bc1ab194181
Matheus Sartori <ms.sartori@outlook.com.br>
2024-04-22 10:26:40 -0300

    feat: change css global colors to match typescript colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      description="Description will go into a meta tag in <head />">
+      description={siteConfig.tagline}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />

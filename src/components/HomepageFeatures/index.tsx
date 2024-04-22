d012ba0baf6a3386fef3c8a4b6c2229b75e84a08
Matheus Sartori <ms.sartori@outlook.com.br>
2024-04-22 10:49:28 -0300

    feat: update home texts and images

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,32 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Algorithm Exploration',
+    Svg: require('@site/static/img/undraw_programmer_re_owql.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Dive into practical examples, exercises, and theory to understand algorithms
+        using TypeScript. Perfect for mastering algorithms for technical interviews.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Practical Examples',
+    Svg: require('@site/static/img/undraw_solution_mindset_re_57bf.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Explore real-world examples with TypeScript, allowing you to focus on understanding
+        the core concepts without worrying about setup or boilerplate code.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'TypeScript Integration',
+    Svg: require('@site/static/img/undraw_developer_activity_re_39tg.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Seamlessly integrate TypeScript into your algorithm studies. Extend or
+        customize your learning experience with the flexibility of TypeScript.
       </>
     ),
   },

9f16e34ed999fb83a3ee42680f8a165cfddff9e3
Matheus Sartori <ms.sartori@outlook.com.br>
2024-04-24 17:53:10 -0300

    feat: run results

--- src/algorithms/editor/editor.tsx
+++ src/algorithms/editor/editor.tsx
@@ -2,12 +2,19 @@ import MonacoEditor from '@monaco-editor/react'
 import { useColorMode } from '@docusaurus/theme-common'
 import { useState } from 'react'
 import { transpile } from 'typescript'
+import SucessMDX from './success.mdx'
+import ErrorMDX from './error.mdx'
+import PendingMDX from './pending.mdx'
 
 interface EditorProps {
   initialCode?: string
+  expectedResult: string
+  returnValue: string
+  args: string
+  editorHeight?: number
 }
 
-export function Editor({ initialCode }: EditorProps) {
+export function Editor({ initialCode, expectedResult, args, returnValue, editorHeight = 300 }: EditorProps) {
   const { colorMode } = useColorMode()
   const [output, setOutput] = useState('')
   const [code, setCode] = useState(initialCode ?? '')
@@ -17,7 +24,7 @@ export function Editor({ initialCode }: EditorProps) {
       const workerCode = `
         onmessage = function(e) {
           try {
-            const result = eval('(' + e.data.code + ')')([1], 1)
+            const result = eval('(' + e.data.code + ')')(${args}, ${returnValue})
             postMessage(result)
           } catch (error) {
             postMessage('Error: ' + error.message)
@@ -47,20 +54,29 @@ export function Editor({ initialCode }: EditorProps) {
 
   return (
     <>
-      <div style={{ display: 'flex', gap: '.5rem' }}>
+      <div style={{ display: 'flex', gap: '.5rem', marginBottom: '1rem' }}>
         <button className="button button--primary" onClick={executeCode}>Run code</button>
       </div>
-      <p>{output}</p>
       <MonacoEditor
-        height={300}
+        height={editorHeight}
         defaultLanguage="typescript"
         theme={colorMode === 'dark' ? 'vs-dark' : 'vs-light'}
         options={{
+          padding: { top: 16 },
           minimap: { enabled: false },
         }}
         value={code}
         onChange={(value) => setCode(value)}
       />
+      <div>
+      {output !== "" && expectedResult == output ? (
+        <SucessMDX />
+      ) : output !== "" && expectedResult != output ? (
+        <ErrorMDX />
+      ) : (
+        <PendingMDX />
+      )}
+      </div>
     </>
   )
 }

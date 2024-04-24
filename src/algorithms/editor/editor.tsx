1626ec67455a19c7b739406af11f2ead3ca57bcd
Matheus Sartori <ms.sartori@outlook.com.br>
2024-04-24 12:11:46 -0300

    feat: run ts code

--- src/algorithms/editor/editor.tsx
+++ src/algorithms/editor/editor.tsx
@@ -1,29 +1,47 @@
 import MonacoEditor from '@monaco-editor/react'
 import { useColorMode } from '@docusaurus/theme-common'
 import { useState } from 'react'
+import { transpile } from 'typescript'
 
-const defaultValue = `function findTreasureIndex (treeBranches, targetBranches) {
-  return -1
-}`
+interface EditorProps {
+  initialCode?: string
+  tests?: string[]
+}
 
-export function Editor () {
+export function Editor ({ initialCode, tests }) {
   const { colorMode } = useColorMode()
   const [output, setOutput] = useState('')
-  const [code, setCode] = useState(defaultValue)
+  const [result, setResult] = useState('')
+  const [code, setCode] = useState(initialCode ?? '')
 
   const executeCode = () => {
     try {
-      const result = eval(`(${code})()`);
+      const jsCode = transpile(code)
+      const result = eval(`(${jsCode})()`);
       setOutput(result);
     } catch (error) {
       setOutput(`Error: ${error.message}`);
     }
-  };
+  }
+
+  const runTests = () => {
+    try {
+      const jsCode = transpile(code)
+      const result = eval(`(${jsCode})([2,4,6,8,10], 6)`)
+      setResult(result)
+    } catch (error) {
+      setResult(`Error: ${error.message}`);
+    }
+  }
 
   return (
     <>
-    <button onClick={executeCode}>Execute</button>
+    <div style={{ display: 'flex', gap: '.5rem' }}>
+      <button className="button button--primary" onClick={executeCode}>Run code</button>
+      <button className="button button--secondary" onClick={runTests}>Submit</button>
+    </div>
     <p>{output}</p>
+    <p>{result}</p>
       <MonacoEditor
         height={300}
         defaultLanguage="typescript"

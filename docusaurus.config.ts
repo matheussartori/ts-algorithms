a8d9622ed6966fd55a619f540335981dea51dd6f
Matheus Sartori <ms.sartori@outlook.com.br>
2024-04-23 21:35:17 -0300

    feat: add social image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -56,7 +56,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/ts-algorithms-social-card.png',
     navbar: {
       title: 'Home',
       logo: {

10ff6e92e98f571d8ce5132156ecb5afc90e6611
Matheus Sartori <ms.sartori@outlook.com.br>
2024-04-25 21:18:35 -0300

    feat: i18n on react

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Layout from '@theme/Layout'
 import HomepageFeatures from '@site/src/components/HomepageFeatures'
 import Heading from '@theme/Heading'
 
+import Translate, {translate} from '@docusaurus/Translate'
 import styles from './index.module.css'
 
 function HomepageHeader() {
@@ -20,7 +21,10 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/getting-started">
-            Start learning now
+            <Translate
+              id="homepage.startLearningNow">
+              Start learning now
+            </Translate>
           </Link>
         </div>
       </div>

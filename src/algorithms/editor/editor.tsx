643054f79879f993daec8c1ee95d7124e08f581e
Matheus Sartori <ms.sartori@outlook.com.br>
2024-05-09 11:40:39 -0300

    feat: test cases

--- src/algorithms/editor/editor.tsx
+++ src/algorithms/editor/editor.tsx
@@ -1,22 +1,29 @@
 import MonacoEditor from '@monaco-editor/react'
 import { useColorMode } from '@docusaurus/theme-common'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { transpile } from 'typescript'
 import Admonition from '@theme/Admonition'
 import Translate, { translate } from '@docusaurus/Translate'
+import styles from './styles.module.css'
 
 interface EditorProps {
   initialCode?: string
   expectedResult: string
-  returnValue: string
   args: string
   editorHeight?: number
+  tests?: Array<{ input: string; output: string }>
 }
 
-export function Editor({ initialCode, expectedResult, args, returnValue, editorHeight = 300 }: EditorProps) {
+interface Test {
+  input: string
+  output: string
+  isCorrect?: boolean
+}
+
+export function Editor({ initialCode, expectedResult, args, tests, editorHeight = 300 }: EditorProps) {
   const { colorMode } = useColorMode()
   const [output, setOutput] = useState('')
-  const [code, setCode] = useState(initialCode ?? '')
+  const codeRef = useRef(initialCode ?? '')
 
   const executeCode = () => {
     try {
@@ -45,19 +52,57 @@ export function Editor({ initialCode, expectedResult, args, returnValue, editorH
         setOutput(e.data)
       }
 
-      worker.postMessage({ code: transpile(code) })
+      worker.postMessage({ code: transpile(codeRef.current) })
     } catch (error) {
       setOutput(`Error: ${error.message}`)
     }
   }
 
+  const executeTests = () => {
+    for (const test of tests) {
+      try {
+        const workerCode = `
+          onmessage = function(e) {
+            try {
+              const result = eval('(' + e.data.code + ')')(${test.input})
+              postMessage(result)
+            } catch (error) {
+              postMessage('Error: ' + error.message)
+            }
+          }
+        `
+
+        const blob = new Blob([workerCode], { type: 'application/javascript' })
+        const workerURL = URL.createObjectURL(blob)
+        const worker = new Worker(workerURL)
+
+        const timeout = setTimeout(() => {
+          worker.terminate()
+          console.error('Error: Timeout reached. Execution took too long.')
+        }, 3000)
+
+        worker.onmessage = (e) => {
+          clearTimeout(timeout)
+          console.log(e.data, test.output)
+        }
+
+        worker.postMessage({ code: transpile(codeRef.current) })
+      } catch (error) {
+        console.error(`Error: ${error.message}`)
+      }
+    }
+  }
+
   return (
     <>
-      <div style={{ display: 'flex', gap: '.5rem', marginBottom: '1rem' }}>
-        <button className="button button--primary" onClick={executeCode}>
+      <div className={styles.container}>
+        <button className="button button--secondary" onClick={executeCode}>
           <Translate id="editor.runCode.text">
             Run code
           </Translate>
+        </button>
+        <button className="button button--primary" onClick={executeTests}>
+          Attempt solution
         </button>
       </div>
       <MonacoEditor
@@ -68,8 +113,8 @@ export function Editor({ initialCode, expectedResult, args, returnValue, editorH
           padding: { top: 16 },
           minimap: { enabled: false },
         }}
-        value={code}
-        onChange={(value) => setCode(value)}
+        value={codeRef.current}
+        onChange={(value) => codeRef.current= value}
       />
       <div style={{ marginTop: '1rem' }}>
       {output !== "" && expectedResult == output ? (
